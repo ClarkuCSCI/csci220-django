0be7dab44dc41e3d7bfd9ef5d283d66cea72a7d5
Peter Story <peter.garth.story@gmail.com>
2024-03-26 10:58:06 -0400

    Scale amount of autogenerated data

--- django/djangoproject/add_fb_bots.py
+++ django/djangoproject/add_fb_bots.py
@@ -9,6 +9,8 @@ django.setup()
 
 from minifacebook.models import Poke, Profile, Status
 
+BATCH_SIZE = 1000
+
 # https://www.ssa.gov/oact/babynames/decades/century.html
 FIRST_NAMES = [
     "Andrew",
@@ -111,56 +113,64 @@ def random_profile():
 def main():
     argparser = argparse.ArgumentParser(description="Add fake Facebook users")
     argparser.add_argument(
-        "--new-profiles",
-        type=int,
-        required=True,
-        help="Number of profiles to add",
+        "--time-limit",
+        type=float,
+        default=120,
+        help="Generate data until the time limit is reached",
     )
     argparser.add_argument(
         "--max-statuses",
         type=int,
-        default=1,
+        default=4,
         help="Each new profile will post up to this many statuses",
     )
     argparser.add_argument(
         "--max-pokes",
         type=int,
-        default=1,
+        default=2,
         help="Each new profile will poke up to this many other users",
     )
     args = argparser.parse_args()
+
+    start_time = timezone.now()
+    start_count = Profile.objects.count()
 
     profile_ids = list(Profile.objects.values_list("id", flat=True))
     # Using a transaction will increase performance, since a commit won't be
     # issued after each operation
     with transaction.atomic():
-        new_profiles = [random_profile() for _ in range(args.new_profiles)]
-        new_profiles = Profile.objects.bulk_create(new_profiles)
-        profile_ids += [p.id for p in new_profiles]
-        new_statuses = []
-        new_pokes = []
-        # For each new profile
-        for p in new_profiles:
-            # Generate random statuses
-            for _ in range(random.randint(1, args.max_statuses)):
-                message = (
-                    f"I'm feeling {random.choice(ADJECTIVES)} "
-                    f"about {random.choice(NOUNS)}!"
-                )
-                new_statuses.append(
-                    Status(profile=p, message=message, date_time=timezone.now())
-                )
-            # Generate random pokes
-            for pokee_id in random.sample(
-                profile_ids,
-                random.randint(1, min(args.max_pokes, len(profile_ids))),
-            ):
-                new_pokes.append(
-                    Poke(poker=p, pokee_id=pokee_id, date_time=timezone.now())
-                )
-        Status.objects.bulk_create(new_statuses)
-        Poke.objects.bulk_create(new_pokes)
-    print(f"Added {args.new_profiles} fake profiles")
+        while (timezone.now() - start_time).total_seconds() < args.time_limit:
+            new_profiles = [random_profile() for _ in range(BATCH_SIZE)]
+            new_profiles = Profile.objects.bulk_create(new_profiles)
+            profile_ids += [p.id for p in new_profiles]
+            new_statuses = []
+            new_pokes = []
+            # For each new profile
+            for p in new_profiles:
+                # Generate random statuses
+                for _ in range(random.randint(1, args.max_statuses)):
+                    message = (
+                        f"I'm feeling {random.choice(ADJECTIVES)} "
+                        f"about {random.choice(NOUNS)}!"
+                    )
+                    new_statuses.append(
+                        Status(
+                            profile=p, message=message, date_time=timezone.now()
+                        )
+                    )
+                # Generate random pokes
+                for pokee_id in random.sample(
+                    profile_ids,
+                    random.randint(1, min(args.max_pokes, len(profile_ids))),
+                ):
+                    new_pokes.append(
+                        Poke(
+                            poker=p, pokee_id=pokee_id, date_time=timezone.now()
+                        )
+                    )
+            Status.objects.bulk_create(new_statuses)
+            Poke.objects.bulk_create(new_pokes)
+    print(f"Added {Profile.objects.count() - start_count} fake profiles")
 
 
 if __name__ == "__main__":
